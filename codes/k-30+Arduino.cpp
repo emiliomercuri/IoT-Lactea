bcf7d7008061d2977cb8421227283f272f79d927
Thiago Schactae de Almeida <thischactae@gmail.com>
2025-12-27 16:24:40 -0300

    Refactor CO2 reading and add error checks
    Refactor CO2 reading logic into a separate function and improve error handling.

--- codes/k-30+Arduino.cpp
+++ codes/k-30+Arduino.cpp
@@ -2,12 +2,7 @@
 
 #define K30_ADDR 0x68
 
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-}
-
-void loop() {
+bool readCO2(int &co2) {
   Wire.beginTransmission(K30_ADDR);
   Wire.write(0x22);
   Wire.write(0x00);
@@ -17,7 +12,8 @@ void loop() {
 
   delay(10);
 
-  Wire.requestFrom(K30_ADDR, 4);
+  int bytes = Wire.requestFrom(K30_ADDR, 4);
+  if (bytes != 4) return false;
 
   byte buffer[4];
   for (int i = 0; i < 4; i++) {
@@ -25,18 +21,29 @@ void loop() {
   }
 
   byte checksum = buffer[0] + buffer[1] + buffer[2];
-  if (checksum != buffer[3]) {
-    Serial.println("{\"error\":\"checksum\"}");
-    delay(2000);
-    return;
+  if (checksum != buffer[3]) return false;
+
+  co2 = ((int)buffer[1] << 8) | buffer[2];
+
+  if (co2 < 250 || co2 > 10000) return false;
+
+  return true;
+}
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  delay(1000); // deixa o sensor estabilizar
+}
+
+void loop() {
+  int co2;
+
+  if (readCO2(co2)) {
+    Serial.print("{\"co2\":");
+    Serial.print(co2);
+    Serial.println("}");
   }
-
-  int co2 = (buffer[1] << 8) | buffer[2];
-
-  Serial.print("{\"co2\":");
-  Serial.print(co2);
-  Serial.println("}");
 
   delay(2000);
 }
-
